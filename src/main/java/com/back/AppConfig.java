f363f07ba0f8f875e4de253a3f5ab3de6620c346
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2026-05-14 22:44:20 +0900

    Component형식으로 변경

--- src/main/java/com/back/AppConfig.java
+++ src/main/java/com/back/AppConfig.java
@@ -5,9 +5,12 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
-	@Bean
-	PersonService personService() {
-		System.out.println("personService 빈 생성됨!");
-		return new PersonService();
-	}
+	/*
+	* 간단한 경우에는 @Service와 같은 자동으로 생성하는걸 이용하는게 더 간편하다
+	* */
+//	@Bean
+//	PersonService personService() {
+//		System.out.println("personService 빈 생성됨!");
+//		return new PersonService();
+//	}
 }
