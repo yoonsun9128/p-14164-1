a5711bf29c72f89ec3cbbc2997d48387e3deb28a
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2026-05-15 10:25:55 +0900

    self 변수를 만ㄴ들기 우해 lazy 필요

--- src/main/java/com/back/AppConfig.java
+++ src/main/java/com/back/AppConfig.java
@@ -1,13 +1,18 @@
 package com.back;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.core.annotation.Order;
 
 @Configuration
 public class AppConfig {
+	@Autowired
+	@Lazy //순환 참조 방지
+	private AppConfig self; //자기 자신을 Bean으로 주입
 	/*
 	* 간단한 경우에는 @Service와 같은 자동으로 생성하는걸 이용하는게 더 간편하다
 	* */
