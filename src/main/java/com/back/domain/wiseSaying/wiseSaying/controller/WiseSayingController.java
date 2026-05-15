033f6f571fb21510c7af17733e0e21025f043326
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2026-05-15 15:23:45 +0900

    수정 기능 추가

--- src/main/java/com/back/domain/wiseSaying/wiseSaying/controller/WiseSayingController.java
+++ src/main/java/com/back/domain/wiseSaying/wiseSaying/controller/WiseSayingController.java
@@ -2,10 +2,7 @@ package com.back.domain.wiseSaying.wiseSaying.controller;
 
 import com.back.domain.wiseSaying.wiseSaying.entity.WiseSaying;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,19 +13,30 @@ import java.util.stream.Collectors;
 //@RequestMapping("wiseSayings")
 public class WiseSayingController {
 	private int lastId = 0;
-	private final List<WiseSaying> wiseSayings = new ArrayList<>();
+//	private final List<WiseSaying> wiseSayings = new ArrayList<>();
+	//테스트 샘플 데이터
+	private final List<WiseSaying> wiseSayings = new ArrayList<>() {{
+		add(new WiseSaying(++lastId, "명언 1", "작가 1"));
+		add(new WiseSaying(++lastId, "명언 2", "작가 2"));
+		add(new WiseSaying(++lastId, "명언 3", "작가 3"));
+		add(new WiseSaying(++lastId, "명언 4", "작가 4"));
+		add(new WiseSaying(++lastId, "명언 5", "작가 5"));
+	}};
 
 	@GetMapping("/wiseSayings/write")
 	@ResponseBody
 	public String write(
+			@RequestParam(defaultValue = "내용")
 			String content,
+			@RequestParam(defaultValue = "작가")
 			String author
 	) {
-		if (content == null || content.isBlank()) {
+//		if (content == null || content.isBlank()) {
+		if (content.isBlank()) { //defaultvalue를 설정해서 null의 조건 삭제
 			throw new IllegalArgumentException("Content cannot be null or blank");
 		}
 
-		if (author == null || author.isBlank()) {
+		if (author.isBlank()) {
 			throw new IllegalArgumentException("author cannot be null or blank");
 		}
 
@@ -66,6 +74,28 @@ public class WiseSayingController {
 		wiseSayings.remove(wiseSaying);
 
 		return "%d번 명언이 삭제되었습니다.".formatted(id);
+	}
+
+	@GetMapping("wiseSayings/modify/{id}")
+	@ResponseBody
+	public String modify(
+			@PathVariable int id,
+			@RequestParam(defaultValue = "") String content,
+			@RequestParam(defaultValue = "") String author
+	) {
+		if (content.isBlank()) {
+			throw new IllegalArgumentException("Content cannot be null or blank");
+		}
+
+		if (author.isBlank()) {
+			throw new IllegalArgumentException("Author cannot be null or blank");
+		}
+
+		WiseSaying wiseSaying = findById(id).orElseThrow(() ->
+				new IllegalArgumentException("%d번 명언은 존재하지 않습니다.".formatted(id))
+		);
+		wiseSaying.modify(content, author);
+		return "%d번 명언이 수정되었습니다.".formatted(id);
 	}
 	private Optional<WiseSaying> findById(int id) {
 		return wiseSayings
--- src/main/java/com/back/domain/wiseSaying/wiseSaying/entity/WiseSaying.java
+++ src/main/java/com/back/domain/wiseSaying/wiseSaying/entity/WiseSaying.java
@@ -12,4 +12,9 @@ public class WiseSaying {
 	private int id;
 	private String content;
 	private String author;
+
+	public void modify(String content, String author) {
+		this.content = content;
+		this.author = author;
+	}
 }
