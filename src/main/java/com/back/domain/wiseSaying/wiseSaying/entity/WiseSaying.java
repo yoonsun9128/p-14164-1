624d6f6c50000d4faaee744df6819de9958ea0d7
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2026-05-15 14:42:52 +0900

    명언 앱 구현 등록 과 조회 추가

---
+++ src/main/java/com/back/domain/wiseSaying/wiseSaying/entity/WiseSaying.java
@@ -0,0 +1,15 @@
+package com.back.domain.wiseSaying.wiseSaying.entity;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class WiseSaying {
+	private int id;
+	private String content;
+	private String author;
+}
